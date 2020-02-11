ee9af8f6954ee0b7e3a80710c7c1ecec8ec356d0
Clive Makamara <clive@makamara.me>
2020-02-11 23:46:44 +0300

    .

--- koha-patched/pull.sh
+++ koha-patched/pull.sh
@@ -6,11 +6,9 @@ if [ -z "$KOHA_RELEASE" ]; then
 	exit 1
 fi
 mkdir -p /koha && cd /koha
-curl -L so /dev/null -w "%{http_code}" "https://github.com/${KOHA_FORK}/Koha/archive/v${KOHA_RELEASE}.tar.gz"
-
-RES=`curl -L so /dev/null -w "%{http_code}" "https://github.com/$KOHA_FORK/Koha/archive/v$KOHA_RELEASE.tar.gz"`
+RES=`curl -sSLk -Iso /dev/null -w "%{http_code}" https://github.com/${KOHA_FORK}/Koha/archive/v${KOHA_RELEASE}.tar.gz`
 if [ $RES -eq 200 ]; then
-  curl -L -sSk -o koha.tar.gz "https://github.com/$KOHA_FORK/Koha/archive/v$KOHA_RELEASE.tar.gz"
+  curl -sSLk -o koha.tar.gz https://github.com/${KOHA_FORK}/Koha/archive/v${KOHA_RELEASE}.tar.gz
 else
   echo "Failed getting tagged release ... giving up!"
   exit 1
