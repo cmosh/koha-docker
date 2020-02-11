268abc744b0aeb76d01c33b6108047d106e1266e
Clive Makamara <clive@makamara.me>
2020-02-11 23:13:48 +0300

    Follow redirects

--- koha-patched/pull.sh
+++ koha-patched/pull.sh
@@ -5,9 +5,9 @@ if [ -z "$KOHA_RELEASE" ]; then
 	echo "Need KOHA_RELEASE"
 	exit 1
 fi
-
+https://github.com/cmosh/Koha/releases/tag/v${KOHA_RELEASE}.tar.gz
 mkdir -p /koha && cd /koha
-RES=`curl -sSk -Iso /dev/null -w "%{http_code}" https://github.com/${KOHA_FORK}/Koha/archive/v${KOHA_RELEASE}.tar.gz`
+RES=`curl -sSLk -Iso /dev/null -w "%{http_code}" https://github.com/${KOHA_FORK}/Koha/archive/v${KOHA_RELEASE}.tar.gz`
 if [ $RES -eq 200 ]; then
   curl -sSk -o koha.tar.gz https://github.com/${KOHA_FORK}/Koha/archive/v${KOHA_RELEASE}.tar.gz
 else
